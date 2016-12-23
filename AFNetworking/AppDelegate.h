1a6f25d831e148aa99a447d21ed0212cf11b82d7
Urvashi-iPhone <lathiyaurvashi36@gmail.com>
2016-12-23 10:22:16 +0530

    Initial Commit

---
+++ AFNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworking
+//
+//  Created by Tecksky Techonologies on 12/23/16.
+//  Copyright © 2016 Tecksky Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
